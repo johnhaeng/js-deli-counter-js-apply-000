d54f57727f0a0f7e0fe25ad88c50ea62f4750e8c
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-20 15:51:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var johnsDeliLine = [];
+var counter = 0;
+
+function takeANumber (numbers) {
+  johnsDeliLine.push(numbers);
+  
+  
+  
+}
 
 //function takeANumber (johnsDeliLine, newPersonName) {   
   //johnsDeliLine.push(newPersonName);
